e989f9cc76681c667e68c3ed03f689e7fa19edc8
amit <amit15jul@gmail.com>
2022-01-09 18:55:07 +0530

    info updated.

--- rpsgame/rpsgame/settings.py
+++ rpsgame/rpsgame/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-mdbe0w474b=c&elhlt2%kt6^z@$!xvjnt8(i(8kurlzi0w)=5f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rpseb-env.eba-bbeqthua.us-west-2.elasticbeanstalk.com', '127.0.0.1']
 
 
 # Application definition
